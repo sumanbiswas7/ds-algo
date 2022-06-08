74c7dea64d3c9d7696b7efd0b478bb088e036dd8
suman <ronaldosuman97@gmail.com>
2022-06-08 16:38:27 +0530

    longest consicutive ones

---
+++ sliding-window/problems/07-longest_ones.py
@@ -0,0 +1,45 @@
+# Longest Subarray with Ones after Replacement 
+
+# Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2
+# Output: 6
+
+def longestOnes(nums, k):
+    char_count = {}
+    win_start = 0
+    max_len = 0
+    one_present = False
+    
+    for win_end in range(len(nums)):
+        char_count[nums[win_end]] = 1 + char_count.get(nums[win_end] , 0)
+        if nums[win_end] == 1: one_present = True
+
+        if one_present:
+            while (win_end-win_start+1) - char_count[1] > k:
+                char_count[nums[win_start]] -= 1
+                win_start += 1
+        
+        max_len = max(max_len , win_end - win_start + 1)
+    
+    return max_len
+
+
+longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3)
+
+#  EASY
+def findMaxConsecutiveOnes(nums):
+    win_start = 0
+    max_len = 0
+    
+    for win_end in range(len(nums)):
+        
+        if nums[win_end] == 0:
+            while nums[win_start] != 1:
+                print(win_start)
+                win_start += 1
+            
+        max_len = max(win_end - win_start + 2, max_len)
+
+    print(max_len)
+    return max_len
+            
+findMaxConsecutiveOnes([1,1,0,1,1,1])
