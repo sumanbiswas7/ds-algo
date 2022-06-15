3fc0b38cc68b4144ca9b90efa6520293c032804a
suman <ronaldosuman97@gmail.com>
2022-06-15 23:35:03 +0530

    remove duplicates -s-nw

--- practice.py
+++ practice.py
@@ -2,14 +2,18 @@ def lenDistinct(nums):
     l , r = 0 , 1
     res = []
     while r < len(nums):
+        print("---------------------")
+        print("l - ",l, " r - ",r,res)
+
         if nums[l] != nums[r]:
-            if len(res) > 0 and res[-1] == nums[r]:
-                res.append(nums[r])
+            if res:
+                if res[-1] == nums[r] or res[-1] == nums[l]:
+                    res.append(nums[r])
             else:
                 res.append(nums[l])
                 res.append(nums[r])
 
-        # elif r == len(nums) - 1 and nums[l] != nums[r]: res.append(nums[r])
+        if r == len(nums) - 1 and nums[l] == nums[r]: res.append(nums[r])
 
 
         l += 1
@@ -20,9 +24,6 @@ def lenDistinct(nums):
 
 
 nums = [2, 3, 3, 3, 6, 9, 9]
+nums = [2, 2, 2, 2, 4, 2]
 lenDistinct(nums)
-
-a = [2]
-if a:
-    print("hi")
 
