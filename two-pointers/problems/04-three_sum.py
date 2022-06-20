b04faba2de543adf6e1bc6bf2f6a90f2ba460704
suman <ronaldosuman97@gmail.com>
2022-06-20 23:23:59 +0530

    three-sum

---
+++ two-pointers/problems/04-three_sum.py
@@ -0,0 +1,35 @@
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
+# such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+
+nums = [-1,0,1,2,-1,-4]
+
+def threeSum(nums):
+    res = []
+    nums.sort()
+
+    for i in range(len(nums)):
+
+        if i != 0 and nums[i] == nums[i-1]: continue
+
+        l , r = i + 1 , len(nums)-1
+        while l < r:
+            threeSum = nums[i] + nums[l] + nums[r]
+            if threeSum > 0:
+                r -= 1
+            elif threeSum < 0:
+                l += 1
+            else:
+                res.append([nums[i],nums[l],nums[r]])
+                l += 1
+                while nums[l] == nums[l-1] and l < r: l+= 1
+
+    print(res)
+
+threeSum(nums)
+
