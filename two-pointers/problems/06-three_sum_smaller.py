7875ca3a04193d7fa2b66dc1566f90c787279760
suman <ronaldosuman97@gmail.com>
2022-07-08 10:19:26 +0530

    add: [two-pointers] three_sum_smallest

---
+++ two-pointers/problems/06-three_sum_smaller.py
@@ -0,0 +1,25 @@
+# Count triplets with sum smaller than X 
+# Given an array arr[] of distinct integers of size N and a value sum, the task is to find the count of triplets (i, j, k),
+# having (i<j<k) with the sum of (arr[i] + arr[j] + arr[k]) smaller than the given value sum.
+
+def triplateWithSmallerSum(nums,target):
+    nums.sort()
+    res = 0
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]: continue
+
+        l , r = i + 1 , len(nums) - 1
+        while l < r:
+            threeSum = nums[i] + nums[l] + nums[r]
+            if threeSum >= target: r -= 1
+            else:
+                print(nums[i],nums[l],nums[r])
+                # Else for current i and j, there can (k-j) possible third elements
+                # that satisfy the constraint.
+                res += r-l
+                l += 1
+    print(res)
+
+# triplateWithSmallerSum([-1,0,2,3], 3)
+triplateWithSmallerSum([-2 ,0 ,1 ,3], 2)
+
