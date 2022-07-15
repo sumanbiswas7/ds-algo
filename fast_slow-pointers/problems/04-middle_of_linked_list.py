67969bac54242aa93c96178b22319e7c1bd0bf64
suman <ronaldosuman97@gmail.com>
2022-07-15 12:16:51 +0530

    add: [fast_slow] middle of the linked list

---
+++ fast_slow-pointers/problems/04-middle_of_linked_list.py
@@ -0,0 +1,16 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    slow, fast = head, head
+    
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        
+    return slow                
+    
+        
+        
+        
